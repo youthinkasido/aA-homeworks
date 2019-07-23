4a63ef8e200bb07b5d1879ee09374c22e29d5bb1
Arshia <youthinkasido@gmail.com>
2019-07-23 11:02:09 -0400

    final project plan commit

---
+++ W14D2/airbnbstore.js
@@ -0,0 +1,23 @@
+{
+    entities: {
+        ar_properties: {
+           1:{ id: 1
+            name: "House in the Wilderness",
+            description: "A beautiful secluded house in the wild",
+            user_id: 2234,
+            property_type_id: 2146,
+            address: "5234 nature avenue"
+            }, 
+
+            2:{ id: 2
+            name: "Timeshare",
+            description: "A peacuful timeshare property",
+            user_id: 1894,
+            property_type_id: 7236,
+            address: "123 Hilton Head Islan"
+            }, 
+    
+
+        }
+    }
+}
